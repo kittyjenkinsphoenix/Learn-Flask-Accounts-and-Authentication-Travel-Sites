ec5a86860909f038bf699f22f3c1e4a1a6980e82
Kitty <k.jenkins-phoenix2@newcastle.ac.uk>
2025-10-29 00:19:22 +0000

    Enhanced Routes With Advanced Features

--- routes.py
+++ routes.py
@@ -1,9 +1,9 @@
-from app import app, db
 from flask import request, render_template, flash, redirect, url_for
+from urllib.parse import urlparse
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
 from models import User, Post
 from forms import RegistrationForm, LoginForm, DestinationForm
-from werkzeug.urls import url_parse
-from flask_login import current_user, login_user, logout_user, login_required
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -18,7 +18,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
@@ -38,6 +38,8 @@ def register():
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
@@ -71,8 +73,66 @@ def user(username):
 
 @app.route('/')
 def index():
-    posts = Post.query.all()
+    posts = Post.query.join(User).\
+        filter(User.id == Post.user_id).\
+        order_by(Post.timestamp.desc()).all()
     return render_template('landing.html', posts=posts)
+
+
+@app.route('/post/<int:post_id>/edit', methods=['POST'])
+@login_required
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    
+    if post.user_id != current_user.id:
+        flash('You can only edit your own posts!')
+        return redirect(url_for('index'))
+    
+    new_description = request.form.get('description')
+    if new_description:
+        post.description = new_description
+        db.session.commit() 
+        flash('Your post has been updated!')
+    
+    return redirect(url_for('user', username=current_user.username))
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    # Example: Deleting records
+    post = Post.query.get_or_404(post_id)
+    
+    if post.user_id != current_user.id:
+        flash('You can only delete your own posts!')
+        return redirect(url_for('index'))
+    
+    db.session.delete(post)  
+    db.session.commit()      
+    flash('Your post has been deleted!')
+    
+    return redirect(url_for('user', username=current_user.username))
+
+
+@app.route('/search')
+def search_posts():
+    query = request.args.get('query', '')
+    country = request.args.get('country', '')
+    
+    posts_query = Post.query
+    
+    if query:
+        posts_query = posts_query.filter(
+            (Post.description.ilike(f'%{query}%')) | 
+            (Post.city.ilike(f'%{query}%'))           
+        )
+    
+    if country:
+        posts_query = posts_query.filter(Post.country.ilike(f'%{country}%'))
+    
+    posts = posts_query.order_by(Post.timestamp.desc()).all()
+    
+    return render_template('landing.html', posts=posts, query=query, country=country)
 
 
 
