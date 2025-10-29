fba9c81e986bc45c8b36a06a80bf4030b4630f4d
Kitty <k.jenkins-phoenix2@newcastle.ac.uk>
2025-10-29 00:19:05 +0000

    Improved Flask Application Structure And Database Initialisation

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
 
+db = SQLAlchemy(app)
 login = LoginManager(app)
-db = SQLAlchemy(app) 
 login.login_view = 'login'
 
-import routes, models
+from routes import *  # This needs to be after db initialization
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create tables if they don't exist
+    app.run(debug=True)
